03f0f4bbfd66705fb6311b7a821650b3bdd78113
Neogara <Neogara519@Gmail.com>
2015-12-04 11:00:00 +0300

    Test

--- Tao_OpenGl_inicialised11/Form1.cs
+++ Tao_OpenGl_inicialised11/Form1.cs
@@ -790,7 +790,7 @@ namespace Tao_OpenGl_inicialised11
                             else
                             {
                                 if (SelectedCube.Y == 1)
-                                {
+                         d       {
                                     SelectedCube.Y += 1;
                                     SelectedCube.Z = 3;
                                 }
@@ -800,7 +800,7 @@ namespace Tao_OpenGl_inicialised11
                         break;
                     }
 
-                case Keys.D:
+                case Keys.D: // test
                     {
                         if (SelectedCube.X == 1)
                         {
